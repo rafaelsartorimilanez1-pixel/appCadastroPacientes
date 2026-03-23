205acce856f000fbf464827f949854aa133072fb
Rafael Sartori Milanez <rafaelsartorimilanez1@gmail.com>
2026-03-23 09:35:49 -0300

    Função delete corrigido

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -10,117 +10,117 @@ const Cadastro = () => {
     const [dadosPacientes, setDadosPacientes] = useState({})
     const [idAtual, setIdAtual] = useState('')
 
-    useEffect(() => {
-        const pacientesRef = ref(database, 'pacientes')
+useEffect(() => {
+    const pacientesRef = ref(database, 'pacientes')
 
-        const unsubscribe = onValue(pacientesRef, (snapshot) => {
-            if (snapshot.val() != null) {
-                setDadosPacientes(snapshot.val())
-            } else {
-                setDadosPacientes({})
-            }
-        })
-
-        return () => unsubscribe()
-    }, [])
-
-    const addEdit = (obj, idAtual) => {
-        const pacientesRef = ref(database, 'pacientes')
-
-        const dadosLimpos = {
-            nomeCompleto: obj.nomeCompleto || '',
-            telefone: obj.telefone || '',
-            email: obj.email || '',
-            endereco: obj.endereco || ''
-        }
-
-        if (idAtual === '') {
-            push(pacientesRef, dadosLimpos)
-                .then(() => {
-                    setIdAtual('')
-                })
-                .catch(console.log)
+    const unsubscribe = onValue(pacientesRef, (snapshot) => {
+        if (snapshot.val() != null) {
+            setDadosPacientes(snapshot.val())
         } else {
-            const pacienteRef = ref(database, `pacientes/${idAtual}`)
-
-            update(pacienteRef, dadosLimpos)
-                .then(() => {
-                    setIdAtual('')
-                })
-                .catch(console.log)
+            setDadosPacientes({})
         }
+    })
+
+    return () => unsubscribe()
+}, [])
+
+const addEdit = (obj, idAtual) => {
+    const pacientesRef = ref(database, 'pacientes')
+
+    const dadosLimpos = {
+        nomeCompleto: obj.nomeCompleto || '',
+        telefone: obj.telefone || '',
+        email: obj.email || '',
+        endereco: obj.endereco || ''
     }
 
-    const deletePaciente = (key) => {
-        if (window.confirm('Deseja realmente deletar esse cadastro?')) {
+    if (idAtual === '') {
+        push(pacientesRef, dadosLimpos)
+            .then(() => {
+                setIdAtual('')
+            })
+            .catch(console.log)
+    } else {
+        const pacienteRef = ref(database, `pacientes/${idAtual}`)
 
-            const pacienteRef = ref(database, `pacientes/${key}`);
+        update(pacienteRef, dadosLimpos)
+            .then(() => {
+                setIdAtual('')
+            })
+            .catch(console.log)
+    }
+}
 
-            remove(pacienteRef)
-                .then(() => {
-                    console.log("Deletado com sucesso");
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-    };
+const deletePaciente = (key) => {
+    if (window.confirm('Deseja realmente deletar esse cadastro?')) {
 
-    return (
-        <div>
-            <div className="p-5 mb-4 bg-light rounded-3">
-                <div className="container-fluid py-5">
-                    <h1 className="display-4">Cadastro de Pacientes</h1>
-                </div>
-            </div>
+        const pacienteRef = ref(database, `pacientes/${key}`);
 
-            <div className='row'>
-                <div className='col-md-5'>
-                    <FormularioCadastro
-                        addEdit={addEdit}
-                        idAtual={idAtual}
-                        dadosPacientes={dadosPacientes}
-                    />
-                </div>
+        remove(pacienteRef)
+            .then(() => {
+                console.log("Deletado com sucesso");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+};
 
-                <div className='col-md-7'>
-                    <table className='table table-bordeless table-striped'>
-                        <thead>
-                            <tr>
-                                <td>Nome</td>
-                                <td>Telefone</td>
-                                <td>Email</td>
-                                <td>Ações</td>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {Object.keys(dadosPacientes).map(id => (
-                                <tr key={id}>
-                                    <td>{dadosPacientes[id].nomeCompleto}</td>
-                                    <td>{dadosPacientes[id].telefone}</td>
-                                    <td>{dadosPacientes[id].email}</td>
-
-                                    <td>
-                                        <button
-                                            className='btn btn-primary'
-                                            onClick={() => setIdAtual(id)}
-                                        >
-                                            <img src={pencil} alt="edit" />
-                                        </button>
-
-                                        <button className='btn btn-danger' onClick={() => deletePaciente(id)}>
-                                            <img src={xcircle} alt="delete" />
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+return (
+    <div>
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-4">Cadastro de Pacientes</h1>
             </div>
         </div>
-    )
+
+        <div className='row'>
+            <div className='col-md-5'>
+                <FormularioCadastro
+                    addEdit={addEdit}
+                    idAtual={idAtual}
+                    dadosPacientes={dadosPacientes}
+                />
+            </div>
+
+            <div className='col-md-7'>
+                <table className='table table-bordeless table-striped'>
+                    <thead>
+                        <tr>
+                            <td>Nome</td>
+                            <td>Telefone</td>
+                            <td>Email</td>
+                            <td>Ações</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {Object.keys(dadosPacientes).map(id => (
+                            <tr key={id}>
+                                <td>{dadosPacientes[id].nomeCompleto}</td>
+                                <td>{dadosPacientes[id].telefone}</td>
+                                <td>{dadosPacientes[id].email}</td>
+
+                                <td>
+                                    <button
+                                        className='btn btn-primary'
+                                        onClick={() => setIdAtual(id)}
+                                    >
+                                        <img src={pencil} alt="edit" />
+                                    </button>
+
+                                    <button className='btn btn-danger' onClick={() => deletePaciente(id)}>
+                                        <img src={xcircle} alt="delete" />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+)
 }
 
 export default Cadastro
